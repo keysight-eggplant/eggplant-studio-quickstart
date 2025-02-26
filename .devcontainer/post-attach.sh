f4f51e6f13472c13729b2786ae836731501812ed
Frederik Carlier <frederik.carlier@keysight.com>
2025-02-26 13:29:34 +0000

    Print welcome message

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -2,3 +2,15 @@
 
 # Make port 8080 visible to the public
 gh codespace ports visibility 8080:public -c $CODESPACE_NAME
+
+# Reset the console
+clear
+
+# Print a welcome message
+
+echo "👋 Welcome to Eggplant Studio!"
+echo
+echo "🔎 To explore Eggplant Studio, open the Command Palette (Cmd/Ctrl + Shift + P) or the Demo Suite."
+echo
+echo "🐞 Create a script, run tests, and rid the world of bad software."
+echo
